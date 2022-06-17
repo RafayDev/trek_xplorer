08816ef7f4aee20f3545a5341b03ffae135bee79
rafay9475 <rafay9475@gmail.com>
2022-06-18 04:06:07 +0500

    add to Favorite

--- lib/pages/user/dashboard.dart
+++ lib/pages/user/dashboard.dart
@@ -1,21 +1,262 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:trek_xplorer/pages/comapny/update_tour.dart';
 
 class Dashboard extends StatefulWidget {
-  Dashboard({Key? key}) : super(key: key);
+  const Dashboard({Key? key}) : super(key: key);
 
   @override
-  _DashboardState createState() => _DashboardState();
+  State<Dashboard> createState() => _DashboardState();
 }
 
+String? email = FirebaseAuth.instance.currentUser!.email;
+
 class _DashboardState extends State<Dashboard> {
+  void initState() {
+    var email = FirebaseAuth.instance.currentUser!.email;
+    super.initState();
+  }
+
+  final Stream<QuerySnapshot> tourStream = FirebaseFirestore.instance
+      .collection('tours')
+      // .where('email', isEqualTo: "$email")
+      .snapshots();
+
+  // For Deleting Tour
+  // CollectionReference favorite =
+  //     FirebaseFirestore.instance.collection('favorites');
+  addfavorite(
+      title, imgUrl, location, date, duration, price, details, id) async {
+    CollectionReference favorite =
+        await FirebaseFirestore.instance.collection('favorites');
+    return favorite
+        .doc(id)
+        .set({
+          'title': title,
+          'location': location,
+          'date': date,
+          'duration': duration,
+          'details': details,
+          'price': price,
+          'email': email,
+          'imgUrl': imgUrl,
+        })
+        .then((value) => print('Tour Added'))
+        .catchError((error) => print('Failed to Add Tour: $error'));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-          child: Text(
-        'Dashboard',
-        style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-      )),
-    );
+    return StreamBuilder<QuerySnapshot>(
+        stream: tourStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            print('Something went Wrong');
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final List storedocs = [];
+          snapshot.data!.docs.map((DocumentSnapshot document) {
+            Map a = document.data() as Map<String, dynamic>;
+            storedocs.add(a);
+            a['id'] = document.id;
+          }).toList();
+          if (storedocs.length == 0) {
+            return Center(
+              child: Text("No Tours Added By You"),
+            );
+          }
+          return ListView(
+            children: [
+              for (var i = 0; i < storedocs.length; i++) ...[
+                Container(
+                  height: 380,
+                  width: 100,
+                  child: Card(
+                    color: Color.fromARGB(225, 241, 116, 221),
+                    child: Column(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(8.0),
+                          child: Image.network(
+                            storedocs[i]['imgUrl'],
+                            width: 390,
+                            height: 150,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Text(
+                                storedocs[i]['title'],
+                                style: DefaultTextStyle.of(context)
+                                    .style
+                                    .apply(fontSizeFactor: 2.0),
+                              ),
+                            ),
+                            Padding(padding: EdgeInsets.all(10.0)),
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Wrap(
+                                children: [
+                                  Icon(
+                                    Icons.location_on,
+                                    color: Colors.black,
+                                  ),
+                                  Text(
+                                    storedocs[i]['location'],
+                                    style: DefaultTextStyle.of(context)
+                                        .style
+                                        .apply(fontSizeFactor: 1.5),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(
+                                        100.0, 0.0, 0.0, 0.0),
+                                  ),
+                                  Icon(
+                                    Icons.date_range,
+                                    color: Colors.black,
+                                  ),
+                                  Text(
+                                    storedocs[i]['date'],
+                                    style: DefaultTextStyle.of(context)
+                                        .style
+                                        .apply(fontSizeFactor: 1.5),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Wrap(
+                                children: [
+                                  Padding(
+                                    padding:
+                                        EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),
+                                  ),
+                                  Text(
+                                    "Duration: " +
+                                        storedocs[i]['duration'] +
+                                        " Days",
+                                    //label: " Days",
+                                    style: DefaultTextStyle.of(context)
+                                        .style
+                                        .apply(fontSizeFactor: 1.5),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(
+                                        20.0, 0.0, 0.0, 0.0),
+                                  ),
+                                  Icon(
+                                    Icons.attach_money,
+                                    color: Colors.black,
+                                  ),
+                                  Text(
+                                    storedocs[i]['price'] + " Rs",
+                                    style: DefaultTextStyle.of(context)
+                                        .style
+                                        .apply(fontSizeFactor: 1.5),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Wrap(
+                                children: [
+                                  Padding(
+                                    padding:
+                                        EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),
+                                  ),
+                                  Text(
+                                    storedocs[i]['details'],
+                                    style: DefaultTextStyle.of(context)
+                                        .style
+                                        .apply(fontSizeFactor: 1.5),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                                padding:
+                                    EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0)),
+                            Row(
+                              //  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Padding(
+                                  padding:
+                                      EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0),
+                                ),
+
+                                ElevatedButton.icon(
+                                    style: ElevatedButton.styleFrom(
+                                      primary: Colors.red,
+                                      shape: new RoundedRectangleBorder(
+                                        borderRadius:
+                                            new BorderRadius.circular(30.0),
+                                      ),
+                                    ),
+                                    icon: Icon(Icons.star),
+                                    onPressed: () async {
+                                      await addfavorite(
+                                          storedocs[i]['title'],
+                                          storedocs[i]['imgUrl'],
+                                          storedocs[i]['location'],
+                                          storedocs[i]['date'],
+                                          storedocs[i]['duration'],
+                                          storedocs[i]['price'],
+                                          storedocs[i]['details'],
+                                          storedocs[i]['id']);
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(
+                                        SnackBar(
+                                          backgroundColor: Colors.red,
+                                          content: Text(
+                                            "Add to Favorite Sucessfully",
+                                            style: TextStyle(fontSize: 20.0),
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                    label: Text("Add Favorites")),
+                                Padding(padding: EdgeInsets.all(10.0)),
+                                ElevatedButton.icon(
+                                    style: ElevatedButton.styleFrom(
+                                      primary: Colors.green,
+                                      shape: new RoundedRectangleBorder(
+                                        borderRadius:
+                                            new BorderRadius.circular(30.0),
+                                      ),
+                                    ),
+                                    icon: Icon(Icons.whatsapp),
+                                    onPressed: () {
+                                      // deleteUser(storedocs[i]['id']);
+                                    },
+                                    label: Text("Contact")),
+                                // child: Text("Delete")),
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    elevation: 10,
+                    margin: EdgeInsets.all(10),
+                  ),
+                ),
+              ],
+            ],
+          );
+        });
   }
 }
