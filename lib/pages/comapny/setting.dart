0e54773c6554b64e20ad93781ed3d00eb16755b6
rafay9475 <rafay9475@gmail.com>
2022-06-22 16:11:51 +0500

    Company Can Change its Whatsapp

--- lib/pages/comapny/setting.dart
+++ lib/pages/comapny/setting.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:trek_xplorer/pages/login.dart';
@@ -11,12 +12,33 @@ class Csetting extends StatefulWidget {
 
 class _CsettingState extends State<Csetting> {
   final _formKey = GlobalKey<FormState>();
+  void initState() {
+    getwhatsapp();
+    super.initState();
+  }
 
+  var whatsapp = "";
   var newPassword = "";
   // Create a text controller and use it to retrieve the current value
   // of the TextField.
-
+  var changeWhatsapp = "";
+  final changeWhatsappController = TextEditingController();
   final newPasswordController = TextEditingController();
+  Future getwhatsapp() async {
+    var collection = FirebaseFirestore.instance
+        .collection('users')
+        .where('email', isEqualTo: FirebaseAuth.instance.currentUser!.email);
+    var querySnapshot = await collection.get();
+    for (var queryDocumentSnapshot in querySnapshot.docs) {
+      Map<String, dynamic> data = queryDocumentSnapshot.data();
+      //  var name = data['name'];
+      //  var phone = data['phone'];
+      whatsapp = data['whatsapp'];
+    }
+    setState(() {
+      whatsapp = whatsapp;
+    });
+  }
 
   @override
   void dispose() {
@@ -26,6 +48,20 @@ class _CsettingState extends State<Csetting> {
   }
 
   final currentUser = FirebaseAuth.instance.currentUser;
+  Future changewhatsapp() {
+    var doc = FirebaseFirestore.instance
+        .collection('users')
+        .where('email', isEqualTo: FirebaseAuth.instance.currentUser!.email)
+        .get();
+    return doc.then((value) {
+      for (var doc in value.docs) {
+        doc.reference.update({
+          'whatsapp': changeWhatsapp,
+        });
+      }
+    });
+  }
+
   changePassword() async {
     try {
       await currentUser!.updatePassword(newPassword);
@@ -54,6 +90,7 @@ class _CsettingState extends State<Csetting> {
         padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
         child: ListView(
           children: [
+            Padding(padding: EdgeInsets.only(top: 20)),
             Container(
               margin: EdgeInsets.symmetric(vertical: 10.0),
               child: TextFormField(
@@ -113,18 +150,142 @@ class _CsettingState extends State<Csetting> {
                 style: TextStyle(fontSize: 18.0),
               ),
             ),
-            ElevatedButton(
-              onPressed: () async => {
-                await FirebaseAuth.instance.signOut(),
-                Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Login(),
+            Padding(padding: EdgeInsets.only(top: 100)),
+            Row(
+              children: [
+                Wrap(
+                  children: [
+                    Icon(Icons.whatsapp),
+                    Padding(padding: EdgeInsets.only(left: 10)),
+                    Text(
+                      whatsapp,
+                      style: TextStyle(fontSize: 25.0),
                     ),
-                    (route) => false)
-              },
-              child: Text('Logout'),
-              style: ElevatedButton.styleFrom(primary: Colors.blueGrey),
+                  ],
+                ),
+                Padding(padding: EdgeInsets.only(left: 50)),
+                IconButton(
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return Dialog(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20)),
+                                elevation: 16,
+                                child: Container(
+                                  height: 200,
+                                  width: 1200,
+                                  child: Column(
+                                    children: <Widget>[
+                                      Icon(Icons.edit,
+                                          size: 40, color: Colors.purple),
+                                      Text(
+                                        "Change Whatsapp Number",
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      TextFormField(
+                                        autofocus: false,
+                                        keyboardType: TextInputType.number,
+                                        controller: changeWhatsappController,
+                                        decoration: InputDecoration(
+                                          labelText: 'Whatsapp: ',
+                                          labelStyle: TextStyle(fontSize: 20.0),
+                                          // border: OutlineInputBorder(),
+                                          icon: Icon(Icons.whatsapp),
+                                          errorStyle: TextStyle(
+                                              color: Colors.redAccent,
+                                              fontSize: 10),
+                                        ),
+                                        //controller: priceController,
+                                      ),
+                                      ElevatedButton.icon(
+                                          onPressed: () async {
+                                            setState(() {
+                                              changeWhatsapp =
+                                                  changeWhatsappController.text;
+                                            });
+                                            ScaffoldMessenger.of(context)
+                                                .showSnackBar(SnackBar(
+                                              backgroundColor: Colors.white,
+                                              content: Center(
+                                                child: Column(
+                                                  children: [
+                                                    Padding(
+                                                        padding: EdgeInsets.all(
+                                                            200.0)),
+                                                    Center(
+                                                      child:
+                                                          CircularProgressIndicator(
+                                                        backgroundColor:
+                                                            Colors.purple,
+                                                      ),
+                                                    ),
+                                                    Padding(
+                                                        padding: EdgeInsets.all(
+                                                            10.0)),
+                                                    Text(
+                                                      "Please Wait...",
+                                                      style: TextStyle(
+                                                        fontSize: 20.0,
+                                                        color: Colors.purple,
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                            ));
+                                            await changewhatsapp();
+                                            await getwhatsapp();
+                                            Navigator.pop(context);
+                                            ScaffoldMessenger.of(context)
+                                                .showSnackBar(
+                                              SnackBar(
+                                                backgroundColor:
+                                                    Colors.orangeAccent,
+                                                content: Text(
+                                                  'Your Whatsapp Number has been Changed !',
+                                                  style:
+                                                      TextStyle(fontSize: 18.0),
+                                                ),
+                                              ),
+                                            );
+                                          },
+                                          icon: Icon(Icons.edit),
+                                          style: ElevatedButton.styleFrom(
+                                              primary: Colors.blue),
+                                          label: Text("Change"))
+                                    ],
+                                  ),
+                                ));
+                          });
+                    },
+                    icon: Icon(Icons.edit)),
+              ],
+            ),
+            Padding(padding: EdgeInsets.only(top: 150)),
+            Wrap(
+              children: [
+                Padding(padding: EdgeInsets.only(left: 120)),
+                ElevatedButton(
+                  onPressed: () async => {
+                    await FirebaseAuth.instance.signOut(),
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Login(),
+                        ),
+                        (route) => false)
+                  },
+                  child: Text(
+                    'Logout',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  style: ElevatedButton.styleFrom(primary: Colors.blueGrey),
+                ),
+              ],
             )
           ],
         ),
